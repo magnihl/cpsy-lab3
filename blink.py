9626d5b70b3e070a6688b3ac93efd93f38a2833a
magnihl <magnihleifs@gmail.com>
2026-09-13 16:18:26 +0000

    blink test alternates green GPIO27 and red GPIO22

--- blink.py
+++ blink.py
@@ -1,4 +1,5 @@
-"""Blink one LED, no sensor. Proves the LED wiring and the pin number.
+"""Blink both LEDs in turn, no sensor. Proves the LED wiring and the pin
+numbers. Green first, then red, half a second each.
 
 Run on the Pi:  ~/cpsy-display-ip/cpsy/bin/python blink.py
 Ctrl+C stops it.
@@ -8,9 +9,13 @@ from time import sleep
 
 from gpiozero import LED
 
-led = LED(27)
+green = LED(27)   # level LED, physical pin 13
+red = LED(22)     # north LED, physical pin 15
+
 while True:
-    led.on()
+    green.on()
+    red.off()
     sleep(0.5)
-    led.off()
+    green.off()
+    red.on()
     sleep(0.5)
